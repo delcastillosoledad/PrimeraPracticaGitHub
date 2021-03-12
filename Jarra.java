2b697a044f2bf018bd439c285b30d4cc9639b07b
Soledad Del Castillo Carrera <soledad@MacBook-Air-de-Soledad.local>
2021-03-12 10:19:33 +0100

    VERSIÓN CON RAMA CAPACIDADCANTIDAD

--- Jarra.java
+++ Jarra.java
@@ -11,3 +11,9 @@ throw new RuntimeException("ERROR: no se pueden crear jarras con capacidad menor
 public void metodoComunitario(){ // Comentario incluido para pruebas
 System.out.println("Aqui escribimos todos"); 
 }
+//capacidadCantidad
+public int capacidad(){
+    return this.capacidad; }
+public int cantidad(){
+    return this.cantidad;
+}
