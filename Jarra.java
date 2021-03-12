940d77ca6c0a0bdc6d17a4b5043435723200e2a4
Soledad Del Castillo Carrera <soledad@MacBook-Air-de-Soledad.local>
2021-03-12 10:06:13 +0100

    PRIMERA VERSIÓN RAMA MÁSTER A

---
+++ Jarra.java
@@ -0,0 +1,13 @@
+public class Jarra {
+private int capacidad;
+private int cantidad; 
+//constructor
+public Jarra(int capacidadInicial){ 
+    if(capacidadInicial<=0){
+throw new RuntimeException("ERROR: no se pueden crear jarras con capacidad menor" + "o igual que cero.");
+} 
+ this.capacidad=capacidadInicial;
+}
+public void metodoComunitario(){ // Comentario incluido para pruebas
+System.out.println("Aqui escribimos todos"); 
+}
