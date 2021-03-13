9c72126caf108d60ceaec385e73873cf0434789c
Rosario Garcia <rosariogar@uma.es>
2021-03-13 12:21:35 +0100

    He editado ya el codigo

--- Jarra.java
+++ Jarra.java
@@ -1,19 +1,30 @@
 public class Jarra {
-private int capacidad;
-private int cantidad; 
-//constructor
-public Jarra(int capacidadInicial){ 
-    if(capacidadInicial<=0){
-throw new RuntimeException("ERROR: no se pueden crear jarras con capacidad menor" + "o igual que cero.");
-} 
- this.capacidad=capacidadInicial;
-}
-public void metodoComunitario(){ // Comentario incluido para pruebas
-System.out.println("Aqui escribimos todos"); 
-}
-//capacidadCantidad
-public int capacidad(){
-    return this.capacidad; }
-public int cantidad(){
-    return this.cantidad;
+    private int capacidad;
+    private int cantidad; 
+
+    //constructor
+    public Jarra(int capacidadInicial){ 
+        if(capacidadInicial<=0){
+            throw new RuntimeException("ERROR: no se pueden crear jarras con capacidad menor" + "o igual que cero.");
+        } 
+        this.capacidad=capacidadInicial;
+    }
+    public void metodoComunitario(){ // Comentario incluido para pruebas
+        System.out.println("Aqui escribimos todos"); 
+    }
+    
+    //capacidadCantidad
+    public int capacidad(){
+        return this.capacidad; }
+    public int cantidad(){
+        return this.cantidad;
+    }
+
+    //llenaVacia
+    public	void	llena(){
+        this.cantidad=this.capacidad;
+    }
+    public	void	vacia(){
+        this.cantidad=0;
+    }
 }
